dc264324758395f6ca47cb560c10797947ae0dc6
Kemboi Cheruiyot <87800637+KolKemboi@users.noreply.github.com>
2023-07-04 19:49:15 +0300

    Update procedure_1.py

--- rotoscoping/procedure_1.py
+++ rotoscoping/procedure_1.py
@@ -7,7 +7,10 @@ image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
 
 edges = cv2.Canny(image, 50, 150)
 cv2.imshow("image", edges)
+
+#saving the image
 out_image = os.path.join("outputs","output_1.png")
 cv2.imwrite(out_image, edges)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
