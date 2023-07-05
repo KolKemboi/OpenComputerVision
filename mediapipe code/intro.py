ca2e4ccae5ba714f55625de146cc6637e9b783bb
Kemboi Cheruiyot <87800637+KolKemboi@users.noreply.github.com>
2023-07-05 16:53:26 +0300

    new file

---
+++ mediapipe code/intro.py
@@ -0,0 +1,58 @@
+import mediapipe as mp
+import cv2
+
+face = cv2.imread("img.jpg")
+gray_face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+
+face_cascade = cv2.CascadeClassifier(
+	cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+)
+
+faces = face_cascade.detectMultiScale(
+	gray_face,
+	scaleFactor = 1.1,
+	minNeighbors = 5
+)
+
+to_put_mask = list()
+
+for (x, y, w, h) in faces:
+	face = face[y: y+h, x: x+w]
+	to_put_mask.append(face)
+
+for face in to_put_mask:
+
+	mp_holistic = mp.solutions.holistic
+
+	holistic_model = mp_holistic.Holistic(
+			min_detection_confidence = 0.5,
+			min_tracking_confidence = 0.5
+		)
+
+	mp_drawing = mp.solutions.drawing_utils
+
+	rgb_face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+
+	rgb_face.flags.writeable = False
+	results = holistic_model.process(rgb_face)
+	rgb_face.flags.writeable = True
+
+	mp_drawing.draw_landmarks(
+		rgb_face,
+		results.face_landmarks,
+		mp_holistic.FACEMESH_CONTOURS,
+		mp_drawing.DrawingSpec(
+			color = (255, 0, 255),
+			thickness = 1,
+			circle_radius = 1
+		),
+		mp_drawing.DrawingSpec(
+			color = (0, 255, 255),
+			thickness = 1,
+			circle_radius = 1
+		)
+	)
+
+cv2.imshow("face", rgb_face)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
